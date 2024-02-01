c67ff97d852fc7e4d046b7b673e4f2f2c1cd4768
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-02-01 14:19:26 +0100

    first commit

---
+++ BetaExpenses/BetaExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaExpensesApp.swift
+//  BetaExpenses
+//
+//  Created by Влад on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BetaExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
