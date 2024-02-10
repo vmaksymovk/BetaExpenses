978e1d8a8c9fadceec96c61fd6f2be80204c1323
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-02-11 00:15:33 +0100

    View updated

--- BetaExpenses/Views/SettingView.swift
+++ BetaExpenses/Views/SettingView.swift
@@ -7,43 +7,35 @@ struct SettingView: View {
     @State private var isPressed : Bool = false
     var body: some View {
         NavigationStack{
-            VStack{
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .frame(width: 140, height: 140)
-                    .foregroundStyle(isPressed ? .cyan : .blue)
-                    .clipShape(Circle())
-//                    .overlay {
-//                        Circle().stroke(Color.gray, lineWidth: 4)
-//                    }
-                    .shadow(radius: 8)
-                
-                
-            }
-            .onTapGesture {
-                isPressed.toggle()
-            }
             
-            .padding(.top, 40)
-            Spacer()
-            VStack{
-                
-                List{
-                    NavigationLink("Add types") {
-                        ListOfTypesView(colorTheme: DefaultColors())
-                            .toolbar{
-                                Button("Add types", systemImage: "plus") {
-                                    isAddtypesPresenting = true
-                                }
+                VStack{
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 140, height: 140)
+                        .foregroundStyle(isPressed ? .cyan : .blue)
+                        .clipShape(Circle())
+                        .shadow(radius: 8)
+                        .onTapGesture {
+                            isPressed.toggle()
+                        }
+                        .padding(.top, 40)
+                        List{
+                            NavigationLink("Add types") {
+                                ListOfTypesView(colorTheme: DefaultColors())
+                                    .toolbar{
+                                        Button("Add types", systemImage: "plus") {
+                                            isAddtypesPresenting = true
+                                        }
+                                    }
+                                    .sheet(isPresented: $isAddtypesPresenting, content: {
+                                        AddTypes(typesItems: types)
+                                    })
+                                
                             }
-                            .sheet(isPresented: $isAddtypesPresenting, content: {
-                                AddTypes(typesItems: types)
-                            })
-                        
+                        }
+                        .navigationTitle("Settings")
                     }
-                }
-                .navigationTitle("Settings")
-            }
+            
         }
     }
 }
